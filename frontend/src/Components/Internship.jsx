880008122b25a93a8a6d973ab78a4618ff20508d
Rajdip <rajdipsaha7697@gmail.com>
2025-10-07 22:06:00 +0530

    adding intern blogs

--- frontend/src/Components/Internship.jsx
+++ frontend/src/Components/Internship.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { Typewriter } from "react-simple-typewriter";
 const Internship = () => {
   return (
     <div>
-      Coming soon...
+     
+       <h1 className="text-5xl md:text-6xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-600">
+                        <Typewriter
+                          words={["Internship 2025"]}
+                          loop={1}
+                          cursor
+                          cursorStyle="|"
+                          typeSpeed={150}
+                          deleteSpeed={80}
+                          delaySpeed={3000}
+                        />
+                      </h1>
       
     </div>
   )
